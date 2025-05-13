b4710a501889c248e7c03f4fe49905785f1a73c1
Mac1618 <ma46099@gmail.com>
2025-05-13 17:20:55 +0800

    feat(lesson5): request params and query

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // imports
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 // rename return value
 const app = express();
@@ -15,9 +15,27 @@ let posts = [
 	{ id: 3, title: 'Orange' },
 ];
 
-// simple get request
-app.get('/api/v1/', (req, res) => {
-	res.json(posts);
+// get all request
+app.get('/api/v1/posts', (req, res) => {
+	const { limit, sort } = req.query;
+	const convertLimitToInt = parseInt(limit);
+	console.log(req.query, limit, sort);
+
+	// should be "number" and "not negative"
+	if (!isNaN(convertLimitToInt) && convertLimitToInt > 0) {
+		res.send(posts.slice(0, convertLimitToInt));
+    
+		// show all posts
+	} else {
+		res.json(posts);
+  }
+});
+
+// get single request
+app.get('/api/v1/post/:postId', (req, res) => {
+	const id = parseInt(req.params.postId);
+
+	res.json(posts.filter((post) => post.id === id));
 });
 
 // running port
